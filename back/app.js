c58622b394ae79f56a4ae560aecd87871bced668
Javier <javilanger@gmail.com>
2019-04-13 15:01:35 -0300

    nuevo master

---
+++ back/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+var path = require("path")
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname + "/public")));
+
+
+app.get('/*', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+  })
+
+app.listen(app.get("port"), () => {
+    console.log(`Listening on port${port}`);
+  });
