5ab18468ab021112238b3b0a759c0a5a0ddc7cb9
angel-er <a.encinasrm@gmail.com>
2019-04-14 12:59:50 -0300

    button

--- front/src/containers/Main.jsx
+++ front/src/containers/Main.jsx
@@ -2,20 +2,24 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { firebaseConnect } from 'react-redux-firebase'
+import { Switch, Route, Link } from 'react-router-dom';
+import Register from './Register';
+import SignIn from './SignIn';
+import Button from "@material-ui/core/Button";
 
 export class Main extends Component {
-
 
     render() {
         return (
             <div>
-                <p>Hola Venezuela</p>
-            </div>
+                <Register history={history} />
+                <SignIn history={history} />
+            </div >
         )
     }
 }
 const mapStateToProps = (state) => ({
-    
+
 })
 
 const mapDispatchToProps = (dispatch) => ({
